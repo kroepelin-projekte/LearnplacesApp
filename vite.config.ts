e05880b525d5c13a100dd023de52247ebafa9418
christof.buch <christof.buch@kroepelin-projekte.de>
2025-04-02 16:54:54 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   esbuild: process.env.NODE_ENV === 'production' ? { drop: ['console', 'debugger'] } : {},
   plugins: [react(), VitePWA({
     strategies: 'injectManifest',
