bd266b60ba051bf62902c0271d26b12b01feaf62
christof.buch <christof.buch@kroepelin-projekte.de>
2025-04-02 17:22:38 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -27,33 +27,6 @@ export default defineConfig({
       display: 'standalone',
       icons: [
         {
-          src: 'icons/Icon-48.png',
-          sizes: '48x48',
-          type: 'image/png',
-        },
-        {
-          src: 'icons/Icon-72.png',
-          sizes: '72x72',
-          type: 'image/png',
-        },
-        {
-          src: 'icons/Icon-96.png',
-          sizes: '96x96',
-          type: 'image/png',
-        },
-        {
-          src: 'icons/Icon-144.png',
-          sizes: '144x144',
-          type: 'image/png',
-        },
-        {
-          src: 'icons/Icon-192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-      ],
- /*     icons: [
-        {
           src: 'pwa-64x64.png',
           sizes: '64x64',
           type: 'image/png',
@@ -68,21 +41,12 @@ export default defineConfig({
           sizes: '512x512',
           type: 'image/png',
         },
-      ],*/
+      ],
     },
 
     injectManifest: {
       globPatterns: ['**/*.{js,css,html,svg,png,jpg,ico}'],
     },
-
-    includeAssets: [
-      'favicon.svg',
-      'icons/Icon-48.png',
-      'icons/Icon-72.png',
-      'icons/Icon-96.png',
-      'icons/Icon-144.png',
-      'icons/Icon-192.png',
-    ],
 
     devOptions: {
       enabled: false,
