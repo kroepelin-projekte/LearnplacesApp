6c86170290a1eeda2203388e22267ffd6d554a61
christof.buch <christof.buch@kroepelin-projekte.de>
2025-05-19 17:18:33 +0200

    testing

--- src/sw.ts
+++ src/sw.ts
@@ -5,7 +5,7 @@ import { NavigationRoute, registerRoute } from 'workbox-routing'
 import {CacheFirst, StaleWhileRevalidate} from 'workbox-strategies';
 import {CacheableResponsePlugin} from 'workbox-cacheable-response';
 import {ExpirationPlugin} from 'workbox-expiration';
-import { getIndexedDBData } from './utils/Database';
+//import { getIndexedDBData } from './utils/Database';
 
 declare let self: ServiceWorkerGlobalScope
 
@@ -31,7 +31,9 @@ registerRoute(new NavigationRoute(
  Custom plugin for workbox stale while revalidate with headers
  =========================================
  */
-/*const jwtTokenPlugin = {
+
+/*
+const jwtTokenPlugin = {
   requestWillFetch: async ({ request }: { request: Request }) => {
     try {
       const accessToken = await getIndexedDBData('access_token');
