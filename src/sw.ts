8d4b8188d007ffb5c2e39c06c3e2d034e183c7f0
christof.buch <christof.buch@kroepelin-projekte.de>
2025-05-28 12:42:54 +0200

    sw testing

--- src/sw.ts
+++ src/sw.ts
@@ -26,6 +26,26 @@ registerRoute(new NavigationRoute(
   { allowlist },
 ))
 
+self.addEventListener('fetch', async (event) => {
+  const cache = await caches.open('page-cache');
+  const match = await cache.match(event.request);
+
+  console.log(`[Service Worker] Prüfe URL: ${event.request.url}`);
+  console.log(`[Service Worker] Gefundene Cache-Response:`, match);
+
+  event.respondWith(
+    match ||
+    fetch(event.request).catch(() => {
+      console.error('[Service Worker] Netzwerkfehler & kein Cache');
+      return new Response(
+        'Die Seite ist derzeit offline und nicht im Cache verfügbar.',
+        { status: 503, statusText: 'Service Unavailable' }
+      );
+    })
+  );
+});
+
+
 /**
  =========================================
  Custom plugin for workbox stale while revalidate with headers
@@ -236,24 +256,6 @@ self.addEventListener("fetch", (event) => {
 });
 
 
-self.addEventListener('fetch', async (event) => {
-  const cache = await caches.open('page-cache');
-  const match = await cache.match(event.request);
-
-  console.log(`[Service Worker] Prüfe URL: ${event.request.url}`);
-  console.log(`[Service Worker] Gefundene Cache-Response:`, match);
-
-  event.respondWith(
-    match ||
-    fetch(event.request).catch(() => {
-      console.error('[Service Worker] Netzwerkfehler & kein Cache');
-      return new Response(
-        'Die Seite ist derzeit offline und nicht im Cache verfügbar.',
-        { status: 503, statusText: 'Service Unavailable' }
-      );
-    })
-  );
-});
 
 
 
